1cac3f7119281f626039c19398ae56ecabbcd515
EDWARD GAIBOR <edward.gaibor001@umb.edu>
2024-03-05 23:21:07 -0500

    feat(testingBench): Added testing bench

--- melanoma/__init__.py
+++
@@ -1,9 +0,0 @@
-# __init__.py
-from .util import Util, NetworkType, DatasetType, ClassType
-from .model import Model
-from .visualizer import Visualizer
-from .cnn import CNN
-from .augmentationStrategy import *
-# from .util import NetworkType
-from .callback import Callback
-from .preprocess import Preprocess
